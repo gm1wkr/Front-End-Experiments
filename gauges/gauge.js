cc74f9c423b625ced304e07a79f86d0a13a82a1b
gm1wkr <gm1wkr@gmail.com>
2022-04-15 23:36:15 +0100

    toward theming template

--- gauges/gauge.js
+++ gauges/gauge.js
@@ -2,12 +2,12 @@ const rawData = [
     {"sid": "temprature", "value": .1},
     {"sid": "humidity", "value": .9},
     {"sid": "cool", "value": .2},
-    {"sid": "coolness", "value": .8},
-    {"sid": "coolness1", "value": .7},
-    {"sid": "coolness2", "value": .3},
-    {"sid": "coolness3", "value": .6},
-    {"sid": "coolness4", "value": 1},
-    {"sid": "coolness5", "value": .0},
+    {"sid": "Hot Hot", "value": .8},
+    {"sid": "Roasting", "value": .7},
+    {"sid": "cChilli", "value": .3},
+    {"sid": "Balmy", "value": .6},
+    {"sid": "Unity", "value": 1},
+    {"sid": "Zero", "value": .0},
 ]
 
 
@@ -16,18 +16,17 @@ const rawData = [
 rawData.forEach(function(num){
     // console.log(num);
     let gaugeTemplate = `
-        
+        <div class="gauge__wrapper">
             <div class="gauge__body">
                 <div class="gauge__fill"></div>
                 <div class="gauge__cover">
                     <span class="dial"><span>${num.value}</span></span>
                 </div>
             </div>
-
             <div class="label">
                 <span class="label-text">Some text</span>
             </div>
-        
+        </div>
     `;
 
 
@@ -48,14 +47,14 @@ rawData.forEach(function(num){
 
 function updateGauge(el,value){
     el.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
-    el.querySelector(".gauge__cover .dial span").textContent = `${Math.round(value * 100)}%`;
+    el.querySelector(".gauge__cover .dial span").textContent = `${Math.round(value * 100)}`;
 
-    let valueDegrees = Math.round(mapRange(value, 1, 100, 1, 120));
+    let valueDegrees = Math.round(mapRange(value, 1, 100, 10, 110));
     // el.querySelector(".gauge__fill").style.filter = `hue-rotate(${valueDegrees}deg)`;
 
-    const bgColor = v => `hsl(${( (1-v) * 120)}, 80%, 25%)`;
+    const bgColor = v => `hsl(${( (1-v) * 200)}, 90%, 35%)`;
     el.querySelector(".gauge__fill").style.backgroundColor = bgColor(value);
-    console.log(`${valueDegrees} -> ${value}  ${bgColor(value)}`);
+    // console.log(`${valueDegrees} -> ${value}  ${bgColor(value)}`);
 }
 
 
@@ -67,22 +66,22 @@ function updateGauge(el,value){
     return Math.round(out);
  }
 
-const coolness = document.getElementById('coolness');
-// setInterval(updateGauge(coolness, 0.9), 2000);
+// const coolness = document.getElementById('coolness');
+// // setInterval(updateGauge(coolness, 0.9), 2000);
 setInterval(
     function(){
         let r = Math.random();
-        setInterval(updateGauge(coolness, r), 2000);
-    },2000);
+        setInterval(updateGauge(cool, r), 3000);
+    },3000);
 
 setInterval(
     function(){
         let r = Math.random();
-        setInterval(updateGauge(humidity, r), 2000);
-    },2000);
+        setInterval(updateGauge(humidity, r), 3000);
+    },3000);
 
     setInterval(
     function(){
         let r = Math.random();
-        setInterval(updateGauge(temprature, r), 2000);
-    },2000);
+        setInterval(updateGauge(temprature, r), 3000);
+    },3000);
